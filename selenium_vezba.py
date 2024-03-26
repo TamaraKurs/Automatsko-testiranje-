01ac77d130db89c39eb653599809bc4ac45d4f85
tamara.mitrovic <tamara.mitrovic@panonit.com>
2024-03-26 14:06:03 +0100

    Prvi test

---
+++ selenium_vezba.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Chrome()
+
+navigationPage = driver.get("https://elearn.smartinit.net/login/index.php")
+username = driver.find_element("id", "username").send_keys("tamara.mitrovic")
+driver.implicitly_wait(10)
+password = driver.find_element("id", "password").send_keys("123")
+login = driver.find_element("id", "loginbtn").click()
+
+
+
+
+
